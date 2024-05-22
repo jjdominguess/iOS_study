0cb9870fe7b16da96dc5d1347b5f0c8f0145d8a5
João José <joaojose.domingues31@gmail.com>
2024-05-21 22:54:19 -0300

    Commit projeto FlashChat'

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class WelcomeViewController: UIViewController {
@@ -14,7 +15,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-")
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+       
+        }
        
     }
     
